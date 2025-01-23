50f15b8f6c8824c5a5b59f1427bc7de2fe568d7f
“Salam7102008” <Shofiul.alam917@gmail.com>
2025-01-23 16:42:17 -0600

    Inside a function use loop & condition to find cheapest phone & highest camera pixel from an array

--- cheapest.js
+++ cheapest.js
@@ -1,4 +1,4 @@
-const phones = [
+/* const phones = [
     {name: 'Samsung', camera: 12, storage: '32gb', price: 36000, color: 'Silver'},
     {name: 'Walton', camera: 10, storage: '32gb', price: 22000, color: 'Red'},
     {name: 'iPhone', camera: 12, storage: '32gb', price: 82000, color: 'Pink'},
@@ -8,21 +8,62 @@ const phones = [
     {name: 'HTC', camera: 12, storage: '32gb', price: 62000, color: 'Dark Silver'},
 ]
 
+
+
+    function cheapestPhone(phones){
+        let cheapest = phones[0];
+        for(let i = 0; i < phones.length; i++){
+            const index = i;
+            const element = phones[index]; */
+
+            // console.log('Element Number:', index, element);
+            /* const phone = phone[i];
+            console.log(phone); */
+    
+            /* if(element.price < cheapest.price){
+                cheapest = element;
+            }
+        }
+        return cheapest;
+    }
+    
+    const mySelection = cheapestPhone(phones);
+    console.log('Cheapest Phone in the array:', mySelection); */
+
+
+
+
+
+
+
+
+const phones = [
+    {name: 'Samsung', camera: 12, storage: '32gb', price: 36000, color: 'Blue'},
+    {name: 'Walton', camera: 10, storage: '32gb', price: 22000, color: 'Red'},
+    {name: 'iPhone', camera: 25, storage: '32gb', price: 82000, color: 'Pink'},
+    {name: 'Xaomi', camera: 17, storage: '32gb', price: 52000, color: 'Gray'},
+    {name: 'Oppo', camera: 7, storage: '32gb', price: 20000, color: 'Black'},
+    {name: 'Nokia', camera: 6, storage: '32gb', price: 44000, color: 'Golden'},
+    {name: 'HTC', camera: 10, storage: '32gb', price: 62000, color: 'Silver'},
+]
+
+
 function cheapestPhone(phones){
-    let cheapest = phones[0];
+    let highestPixel = phones[0];
     for(let i = 0; i < phones.length; i++){
         const index = i;
         const element = phones[index];
+        
         // console.log('Element Number:', index, element);
         /* const phone = phone[i];
         console.log(phone); */
 
-        if(element.price < cheapest.price){
-            cheapest = element;
+        if(element.camera > highestPixel.camera){
+            highestPixel = element;
         }
     }
-    return cheapest;
+    return highestPixel;
 }
 
 const mySelection = cheapestPhone(phones);
-console.log(mySelection);
+console.log('Highest Pixel Camera in the array:', mySelection);
